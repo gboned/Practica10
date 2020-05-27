f2ea29f7c977370e87ea87842d5a12e49ef69c2b
gboned <gboned@cifpfbmoll.eu>
2020-05-27 20:22:25 +0200

    Añadir un parámetro más a la clase Animal

--- js/funciones.js
+++ js/funciones.js
@@ -7,10 +7,11 @@
 // Creo la clase principal.
 class Animal {
     // Añado el constructor de la clase.
-    constructor(especie, clase, numeroPatas, genero) {
+    constructor(especie, clase, numeroPatas, genero, sonido) {
         this.especie = especie;
         this.clase = clase;
         this.numeroPatas = numeroPatas;
         this.genero = genero;
+        this.sonido = sonido;
     }
 }

be6e28da45dceae9051f140a272c437728ee6449
gboned <gboned@cifpfbmoll.eu>
2020-05-27 20:21:45 +0200

    Añadir clase principal

--- js/funciones.js
+++ js/funciones.js
@@ -4,4 +4,13 @@
 /*                                    */
 /**************************************/
 
-
+// Creo la clase principal.
+class Animal {
+    // Añado el constructor de la clase.
+    constructor(especie, clase, numeroPatas, genero) {
+        this.especie = especie;
+        this.clase = clase;
+        this.numeroPatas = numeroPatas;
+        this.genero = genero;
+    }
+}
